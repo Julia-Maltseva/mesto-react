0626a876ed63e829422094c9594970fbfd1f6974
Julia Maltseva <yuliya-maltseva93@yandex.ru>
2023-02-01 21:59:48 +0300

    Практическая работа 10

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-  const [selectedCard, setSelectedCard] = React.useState(false);
+  const [selectedCard, setSelectedCard] = React.useState(null);
   const [cards, setCards] = React.useState([])
 
   function handleEditProfileClick() {
@@ -30,7 +30,7 @@ function App() {
     setIsEditProfilePopupOpen(false)
     setIsAddPlacePopupOpen(false)
     setIsEditAvatarPopupOpen(false)
-    setSelectedCard(false)
+    setSelectedCard(null)
   }
 
   function handleCardClick(card) {
